97de029af78e77faaf1d72b6ebd833d047febeca
Deepanshu2906 <deepanshugoyal2906@gmail.com>
2023-05-23 17:58:49 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -3,11 +3,14 @@ const { City } = require("../models/index");
 
 class CityRepository {
   async createCity({ name }) {
-    // destruturing obj
+    //{ name:"New delhi"}
     try {
-      const city = await City.create({ name });
+      const city = await City.create({
+        name, // name : name
+      });
       return city;
     } catch (error) {
+      console.log("Something went wrong in the repositiry layer");
       throw { error };
     }
   }
@@ -18,7 +21,32 @@ class CityRepository {
           id: cityId,
         },
       });
+      return true;
     } catch (error) {
+      console.log("Something went wrong in the repositiry layer");
+      throw { error };
+    }
+  }
+  async updateCity(cityId, data) {
+    try {
+      const city = await City.update(data, {
+        where: {
+          id: cityId,
+        },
+      });
+      return city;
+    } catch (error) {
+      console.log("Something went wrong in the repositiry layer");
+      throw { error };
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPk(cityId);
+      return city;
+    } catch (error) {
+      console.log("Something went wrong in the repositiry layer");
       throw { error };
     }
   }
