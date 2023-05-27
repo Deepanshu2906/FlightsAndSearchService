bc4a61038a710b57ef2bd07acbe29a1028deb5bc
Deepanshu2906 <deepanshugoyal2906@gmail.com>
2023-05-27 12:13:02 +0530

    Added flight-create request middleware

---
+++ src/middlewares/flight-middlewares.js
@@ -0,0 +1,24 @@
+const validateCreateFlight = (req, res, next) => {
+  if (
+    (!req.body.flightNumber ||
+      !req.body.airplaneId ||
+      !req.body.departureAirportId,
+    !req.body.arrivalAirportId,
+    !req.body.arrivalTime,
+    !req.body.departureTime,
+    !req.body.price)
+  ) {
+    // if any of the body params is missing we come inside tif
+    return res.status(400).json({
+      data: {},
+      success: false,
+      message: "Invalid request body for create flight",
+      err: "missing mandatory properties for creating flight",
+    });
+  }
+  next();
+};
+
+module.exports = {
+  validateCreateFlight,
+};
